f530283ef0454614fe2d0866699fc20c9484412e
Mark Smith <mcs@pearlcrescent.com>
2020-05-05 16:20:28 -0400

    Avoid adding an extension of .null on Android
    If the system lacks a mapping for the file's MIME type, skip
    adding an extension. The filename option may be used to specify
    a custom name with an extension in this case.

--- android/src/main/java/cl/json/ShareFile.java
+++ android/src/main/java/cl/json/ShareFile.java
@@ -108,12 +108,16 @@ public class ShareFile {
         if(this.isBase64File()) {
             String encodedImg = this.uri.getSchemeSpecificPart().substring(this.uri.getSchemeSpecificPart().indexOf(";base64,") + 8);
             String filename = this.filename != null ? this.filename : System.nanoTime() + "";
+            // Only add an extension if the system has a mapping for this file's MIME type.
+            if (extension != null) {
+              filename = filename + "." + extension;
+            }
             try {
                 File dir = new File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_DOWNLOADS );
                 if (!dir.exists() && !dir.mkdirs()) {
                     throw new IOException("mkdirs failed on " + dir.getAbsolutePath());
                 }
-                File file = new File(dir, filename + "." + extension);
+                File file = new File(dir, filename);
                 final FileOutputStream fos = new FileOutputStream(file);
                 fos.write(Base64.decode(encodedImg, Base64.DEFAULT));
                 fos.flush();
